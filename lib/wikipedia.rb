e4aaa8234be306bc5b28e913c76c39f72e9268e4
Fukumoto Atsushi <fukumoto@imasy.or.jp>
2015-03-11 00:48:16 +0900

    initial commit

---
+++ lib/wikipedia.rb
@@ -0,0 +1,12 @@
+
+require 'wikipedia/client.rb'
+
+module Wikipedia
+  API_URL_LANG = {
+    "en" => "https://en.wikipedia.org/w/api.php"
+  }
+
+  API_URL = API_URL_LANG["en"]
+end
+
+
---
+++ lib/wikipedia/version.rb
@@ -0,0 +1,3 @@
+module Wikipedia
+  VERSION = '0.0.1'
+end
